9c0ad3e565042ef3d6a0a9e4aef2129849d0108e
kjmsapp <kjmsapp@gmail.com>
2022-04-16 18:45:04 +0900

    리빌드
    최신 빌드가 아닌것으로 확인. 리빌드 커밋.

---
+++ precache-manifest.d9c74225ee32d3e65d60d17bc9e6a210.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba95ac744501f3588964",
+    "url": "/css/app.9808a896.css"
+  },
+  {
+    "revision": "ba95ac744501f3588964",
+    "url": "/js/app.e849bb97.js"
+  },
+  {
+    "revision": "991fece1f8aa1d8149f8",
+    "url": "/css/chunk-vendors.b8289af1.css"
+  },
+  {
+    "revision": "991fece1f8aa1d8149f8",
+    "url": "/js/chunk-vendors.94613d30.js"
+  },
+  {
+    "revision": "0741a591b1a1c0d3d837015e742f67d9",
+    "url": "/img/ms1.0741a591.jpg"
+  },
+  {
+    "revision": "a3d99d53c5f4335bdf4c3172c54ef63d",
+    "url": "/img/icon.a3d99d53.png"
+  },
+  {
+    "revision": "24b57b6942232173ea4d5887fb9f8343",
+    "url": "/img/mountains1.24b57b69.jpg"
+  },
+  {
+    "revision": "47f92173344422e6c6f46e9c86676e19",
+    "url": "/img/mountains2.47f92173.jpg"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/fonts/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/fonts/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/fonts/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "55242ea5c378da626cab5b4aaf9c2255",
+    "url": "/fonts/MaterialIcons-Regular.55242ea5.ttf"
+  },
+  {
+    "revision": "21ccf3d35e75314f4134cc27446d556c",
+    "url": "/img/ms3.21ccf3d3.jpg"
+  },
+  {
+    "revision": "e94e24ee84abd89b33e924046895e5b0",
+    "url": "/img/ms2.e94e24ee.jpg"
+  },
+  {
+    "revision": "b6b5f2a90021599df1e1d8946c73a434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8be31c7393404405610908f7148d857",
+    "url": "/js/imgalign.worker.js"
+  },
+  {
+    "revision": "bf77b6bb49743eda4f9722cf84c75e2c",
+    "url": "/js/opencv_3_4_custom_Oz.js"
+  },
+  {
+    "revision": "40f2b15034b7797ba9fbdcfdae2fd465",
+    "url": "/js/opencv_3_4_custom_O3.js"
+  }
+];
--- workbox-sw.js
+++ workbox-sw.js
@@ -1,16 +1,16 @@
-importScripts("/precache-manifest.4c7bb91faf717866b80d08477ddd4bb5.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.d9c74225ee32d3e65d60d17bc9e6a210.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
-//workbox.core.setCacheNameDetails({prefix: "imgalign"});
+workbox.core.setCacheNameDetails({prefix: "imgalign"});
 
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 
-workbox.routing.registerNavigationRoute('/index.html');
+workbox.routing.registerNavigationRoute('/imgalign/index.html');
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.googleAnalytics.initialize();
+//workbox.googleAnalytics.initialize();
 
 self.addEventListener('install', () => {
   self.skipWaiting();
